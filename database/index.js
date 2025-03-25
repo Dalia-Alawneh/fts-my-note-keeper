5dd656622d58ce85c8435c5d4dd78ae3e1161070
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-03-25 12:41:08 +0200

    Add env variables

--- database/index.js
+++ database/index.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
-const connectDB = async()=>{
-    return await mongoose.connect('mongodb://localhost:27017')
+const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.DB_URI)
+    console.log(`✅ MongoDB Connected to ${connection.host}`);
+  } catch (e) {
+    console.error(`❌ Error connecting to MongoDB: ${e.message}`);
+  }
 }
 
 export default connectDB
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import connectDB from './database/index.js';
-
+import 'dotenv/config'
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+connectDB()
 app.use(express.json());
 app.use(cors());
-connectDB()
-
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT} ..`);
 })
