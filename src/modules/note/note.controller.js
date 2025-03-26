03651dc49e85436f0511b587791a91da9b38f0d1
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-03-26 11:10:36 +0200

    Implement update and delete

--- src/modules/note/note.controller.js
+++ src/modules/note/note.controller.js
@@ -6,16 +6,16 @@ const createNote = async (req, res) => {
     return res.status(201).json({ message: "Note added successfully", addedNote });
   } catch (e) {
     console.error("Error creating note:", e);
-    
+
     return res.status(500).json({ message: "Failed to add note", error: e.message });
   }
 }
 
 const getNotes = async (req, res) => {
-  try{
+  try {
     const notes = await Note.find();
     return res.json({ notes })
-  }catch(e){
+  } catch (e) {
     console.error("Error fetching notes:", e);
 
     return res.status(500).json({ message: "Failed to fetch notes", error: e.message });
@@ -23,14 +23,73 @@ const getNotes = async (req, res) => {
 }
 
 const getNote = async (req, res) => {
-  try{
-    const note = await Note.findById(req.params.id);
-    if(!note){
-      return res.status(404).json({message: "Note not found"})
+  const { id } = req.params;
+  try {
+    const note = await Note.findById(id);
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" })
     }
 
     return res.status(200).json({ note })
-  }catch(e){
+  } catch (e) {
+    console.error("Error fetching note:", e);
+
+    return res.status(500).json({ message: "Failed to fetch note", error: e.message });
+  }
+}
+
+const deleteNote = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await Note.findByIdAndDelete(id);
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" })
+    }
+
+    return res.status(200).json({ message: "Note deleted successfully" })
+  } catch (e) {
+    console.error("Error fetching note:", e);
+
+    return res.status(500).json({ message: "Failed to fetch note", error: e.message });
+  }
+}
+
+const updateNote = async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  try {
+    const note = await Note.findOneAndReplace(
+      { _id: id },
+      { title, content },
+      { new: true }
+    );
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" })
+    }
+
+    return res.status(200).json({ message: "Note updated successfully", note })
+  } catch (e) {
+    console.error("Error fetching note:", e);
+
+    return res.status(500).json({ message: "Failed to fetch note", error: e.message });
+  }
+}
+
+const partiallyUpdateNote = async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  try {
+    const note = await Note.findOneAndUpdate(
+      { _id: id },
+      { title, content },
+      { new: true }
+    );
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" })
+    }
+
+    return res.status(200).json({ message: "Note updated successfully", note })
+  } catch (e) {
     console.error("Error fetching note:", e);
 
     return res.status(500).json({ message: "Failed to fetch note", error: e.message });
@@ -38,4 +97,4 @@ const getNote = async (req, res) => {
 }
 
 
-export { createNote, getNotes, getNote }
+export { createNote, getNotes, getNote, deleteNote, updateNote, partiallyUpdateNote }
--- src/modules/note/note.router.js
+++ src/modules/note/note.router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createNote, getNote, getNotes } from "./note.controller.js";
+import { createNote, deleteNote, getNote, getNotes, partiallyUpdateNote, updateNote } from "./note.controller.js";
 
 const router = Router();
 router.post('/', createNote);
 router.get('/', getNotes);
 router.get('/:id', getNote);
+router.delete('/:id', deleteNote);
+router.put('/:id', updateNote);
+router.patch('/:id', partiallyUpdateNote);
 
 export default router;
